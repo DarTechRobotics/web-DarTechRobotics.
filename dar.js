e610f77f9a7ed4b8ec8e2409587998de1f635c44
DarTechRobotics <elian1darwin1@gmail.com>
2024-12-22 13:55:43 -0500

    Add files via upload

---
+++ dar.js
@@ -0,0 +1,14 @@
+TXimg = function() {
+    let cmpImagen = document.getElementById('Imagen');
+    cmpImagen.src ='conoce.png';
+}
+
+TXimg1 = function() {
+    let cmpImagen = document.getElementById('Imagen2');
+    cmpImagen.src = 'imagen2.png';
+}
+
+TXimg2 = function() {
+    let cmpImagen = document.getElementById('Imagen3');
+    cmpImagen.src = 'imagen4.0.png.jpeg';
+}
